2e2bc034ee9079cbd342ea3d76612c345ec3eeb6
kokke <spam@rowdy.dk>
2017-11-05 00:14:46 +0100

    Add files via upload

---
+++ ecdh.h
@@ -0,0 +1,108 @@
+/* 
+
+  Crypto using elliptic curves defined over the finite binary field GF(2^m) where m is prime.
+
+  The curves used are the anomalous binary curves (ABC-curves) or also called Koblitz curves.
+
+  This class of curves was chosen because it yields efficient implementation of operations.
+
+
+
+  Curves available - their different NIST/SECG names and eqivalent symmetric security level:
+
+      NIST      SEC Group     strength
+    ------------------------------------
+      K-163     sect163k1      80 bit
+      B-163     sect163r2      80 bit
+      K-233     sect233k1     112 bit
+      B-233     sect233r1     112 bit
+      K-283     sect283k1     128 bit
+      B-283     sect283r1     128 bit
+      K-409     sect409k1     192 bit
+      B-409     sect409r1     192 bit
+      K-571     sect571k1     256 bit
+      B-571     sect571r1     256 bit
+
+
+
+  Curve parameters from:
+
+    http://www.secg.org/sec2-v2.pdf
+    http://csrc.nist.gov/publications/fips/fips186-3/fips_186-3.pdf
+
+
+  NIST Test Vectors from:
+
+    http://point-at-infinity.org/ecc/nisttv
+    https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/ECDSA_Characteristic2.pdf
+
+
+  References:
+
+    https://www.ietf.org/rfc/rfc4492.txt 
+    http://www.ecrypt.eu.org/ecrypt2/documents/D.SPA.20.pdf
+    https://www.ipa.go.jp/security/enc/CRYPTREC/fy15/doc/1029_report.pdf
+
+*/
+
+#ifndef _ECC_H__
+#define _ECC_H__
+
+
+/* for size-annotated integer types: uint8_t, uint32_t etc. */
+#include <stdint.h> 
+
+#define NIST_B163  1
+#define NIST_K163  2
+#define NIST_B233  3
+#define NIST_K233  4
+#define NIST_B283  5
+#define NIST_K283  6
+#define NIST_B409  7
+#define NIST_K409  8
+#define NIST_B571  9
+#define NIST_K571 10
+
+/* What is the default curve to use? */
+#ifndef ECC_CURVE
+ #define ECC_CURVE NIST_B163
+#endif
+
+#if defined(ECC_CURVE) && (ECC_CURVE != 0)
+ #if   (ECC_CURVE == NIST_K163) || (ECC_CURVE == NIST_B163)
+  #define CURVE_DEGREE       163
+  #define ECC_PRV_KEY_SIZE   21
+ #elif (ECC_CURVE == NIST_K233) || (ECC_CURVE == NIST_B233)
+  #define CURVE_DEGREE       233
+  #define ECC_PRV_KEY_SIZE   30
+ #elif (ECC_CURVE == NIST_K283) || (ECC_CURVE == NIST_B283)
+  #define CURVE_DEGREE       283
+  #define ECC_PRV_KEY_SIZE   36
+ #elif (ECC_CURVE == NIST_K409) || (ECC_CURVE == NIST_B409)
+  #define CURVE_DEGREE       409
+  #define ECC_PRV_KEY_SIZE   52
+ #elif (ECC_CURVE == NIST_K571) || (ECC_CURVE == NIST_B571)
+  #define CURVE_DEGREE       571
+  #define ECC_PRV_KEY_SIZE   72
+ #endif
+#else
+ #error Must define a curve to use
+#endif
+
+#define ECC_PUB_KEY_SIZE     (2 * ECC_PRV_KEY_SIZE)
+
+
+/******************************************************************************/
+
+
+
+int ecdh_generate_keys(uint8_t* public, uint8_t* private);
+int ecdh_shared_secret(const uint8_t* private, const uint8_t* other_pub, uint8_t* output);
+
+
+
+/******************************************************************************/
+
+
+#endif /* #ifndef _ECC_H__ */
+
---
+++ ecdh_example.c
@@ -0,0 +1,140 @@
+/*
+  Diffie-Hellman key exchange (without HMAC) aka ECDH_anon in RFC4492
+
+
+  1. Alice picks a (secret) random natural number 'a', calculates P = a * G and sends P to Bob.
+     'a' is Alice's private key. 
+     'P' is Alice's public key.
+
+  2. Bob picks a (secret) random natural number 'b', calculates Q = b * G and sends Q to Alice.
+     'b' is Bob's private key.
+     'Q' is Bob's public key.
+
+  3. Alice calculates S = a * Q = a * (b * G).
+
+  4. Bob calculates T = b * P = b * (a * G).
+
+  .. which are the same two values since multiplication in the field is commutative and associative.
+
+  T = S = the new shared secret.
+
+
+  Pseudo-random number generator inspired from: http://burtleburtle.net/bob/rand/smallprng.html
+
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ecc.h"
+
+
+
+/* pseudo random number generator with 128 bit internal state... probably not suited for cryptographical usage */
+typedef struct
+{
+  uint32_t a;
+  uint32_t b;
+  uint32_t c;
+  uint32_t d;
+} prng_t;
+
+static prng_t prng_ctx;
+
+static uint32_t prng_rotate(uint32_t x, uint32_t k)
+{
+  return (x << k) | (x >> (32 - k)); 
+}
+
+static uint32_t prng_next(void)
+{
+  uint32_t e = prng_ctx.a - prng_rotate(prng_ctx.b, 27); 
+  prng_ctx.a = prng_ctx.b ^ prng_rotate(prng_ctx.c, 17); 
+  prng_ctx.b = prng_ctx.c + prng_ctx.d;
+  prng_ctx.c = prng_ctx.d + e; 
+  prng_ctx.d = e + prng_ctx.a;
+  return prng_ctx.d;
+}
+
+static void prng_init(uint32_t seed)
+{
+  uint32_t i;
+  prng_ctx.a = 0xf1ea5eed;
+  prng_ctx.b = prng_ctx.c = prng_ctx.d = seed;
+
+  for (i = 0; i < 31; ++i) 
+  {
+    (void) prng_next();
+  }
+}
+
+
+
+
+
+static void ecdh_demo(void)
+{
+  static uint8_t puba[ECC_PUB_KEY_SIZE];
+  static uint8_t prva[ECC_PRV_KEY_SIZE];
+  static uint8_t seca[ECC_PUB_KEY_SIZE];
+  static uint8_t pubb[ECC_PUB_KEY_SIZE];
+  static uint8_t prvb[ECC_PRV_KEY_SIZE];
+  static uint8_t secb[ECC_PUB_KEY_SIZE];
+  uint32_t i;
+
+  /* 0. Initialize and seed random number generator */
+  static int initialized = 0;
+  if (!initialized)
+  {
+    prng_init((0xbad ^ 0xc0ffee ^ 42) | 0xcafebabe | 666);
+    initialized = 1;
+  }
+
+  /* 1. Alice picks a (secret) random natural number 'a', calculates P = a * g and sends P to Bob. */
+  for (i = 0; i < ECC_PRV_KEY_SIZE; ++i)
+  {
+    prva[i] = prng_next();
+  }
+  assert(ecdh_generate_keys(puba, prva));
+
+  /* 2. Bob picks a (secret) random natural number 'b', calculates Q = b * g and sends Q to Alice. */
+  for (i = 0; i < ECC_PRV_KEY_SIZE; ++i)
+  {
+    prvb[i] = prng_next();
+  }
+  assert(ecdh_generate_keys(pubb, prvb));
+
+  /* 3. Alice calculates S = a * Q = a * (b * g). */
+  assert(ecdh_shared_secret(prva, pubb, seca));
+
+  /* 4. Bob calculates T = b * P = b * (a * g). */
+  assert(ecdh_shared_secret(prvb, puba, secb));
+
+  /* 5. Assert equality, i.e. check that both parties calculated the same value. */
+  for (i = 0; i < ECC_PUB_KEY_SIZE; ++i)
+  {
+    assert(seca[i] == secb[i]);
+  }
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  int i;
+  int ncycles = 1;
+
+  if (argc > 1)
+  {
+    ncycles = atoi(argv[1]);
+  }
+
+  for (i = 0; i < ncycles; ++i)
+  {
+    ecdh_demo();
+  }
+
+  return 0;
+}
+
+
