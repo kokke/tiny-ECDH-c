1386bd08b495addd6946b8de714e8a9d18417ed2
kokke <spam@rowdy.dk>
2017-11-05 02:52:57 +0100

    Update ecdh.c

--- ecdh.c
+++ ecdh.c
@@ -1,23 +1,27 @@
 /* 
 
-  Public key crypto using elliptic curves over the finite binary field GF(2^m) where m is prime.
+  Crypto using elliptic curves defined over the finite binary field GF(2^m) where m is prime.
 
-  Supporting 10 NIST / SEC Group's Koblitz curves and Pseudo-Random curves.
+  The curves used are the anomalous binary curves (ABC-curves) or also called Koblitz curves.
 
-  Curves available (NIST/SECG names)
+  This class of curves was chosen because it yields efficient implementation of operations.
 
-      NIST      SEC Group
-    -----------------------
-      K-163     sect163k1
-      B-163     sect163r2
-      K-233     sect233k1
-      B-233     sect233r1
-      K-283     sect283k1
-      B-283     sect283r1
-      K-409     sect409k1
-      B-409     sect409r1
-      K-571     sect571k1
-      B-571     sect571r1
+
+
+  Curves available - their different NIST/SECG names and eqivalent symmetric security level:
+
+      NIST      SEC Group     strength
+    ------------------------------------
+      K-163     sect163k1      80 bit
+      B-163     sect163r2      80 bit
+      K-233     sect233k1     112 bit
+      B-233     sect233r1     112 bit
+      K-283     sect283k1     128 bit
+      B-283     sect283r1     128 bit
+      K-409     sect409k1     192 bit
+      B-409     sect409r1     192 bit
+      K-571     sect571k1     256 bit
+      B-571     sect571r1     256 bit
 
 
 
@@ -28,9 +32,8 @@
 
 
   Reference:
-  
-    https://www.ietf.org/rfc/rfc4492.txt 
 
+    https://www.ietf.org/rfc/rfc4492.txt 
 */
 
 #include <stdint.h>
